1a28405f3a54ea5bc496c701eba91f201e8a1147
Jembudmerah <113617150+Jembudmerah@users.noreply.github.com>
2024-02-12 20:46:24 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/Jembudmerah/vps/main/limit/bot.zip
+wget https://raw.githubusercontent.com/Jembudmerah/Purel/main/limit/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/Jembudmerah/vps/main/limit/kyt.zip
+wget https://raw.githubusercontent.com/Jembudmerah/Purel/main/limit/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
